c27dc1eb34bc352387edc8fb86915a92bb4b6c5c
Michael Towson <MTowson@gmail.com>
2021-10-26 17:27:29 -0700

    add toast to login/registration

--- README.md
+++ README.md
@@ -1,31 +1,44 @@
----ABOUT---
+|ABOUT| --
+
 This website was originally completed in partial fulfillment of requirements for Full-Stack Certification at Coding Dojo under the title "Resort-App."
 
 This code base has been refactored, expanded, and enhanced by Michael Towson, one of the original authors.
 
---.ENV REQUIREMENTS---
+|.ENV REQUIREMENTS| --
+
 Server will not run without .env file, which is not included in public repository. To obtain, email mtowsoncoding@gmail.com or construct your own .env file in the server folder with the following variables:
   
   PORT={#}
+
   SESS_SECRET={yourSessionSecret}
+
   COOKIE_NAME={'your-cookie-name AS STRING'}
+
   MONGO_URI{'mongodb atlas url AS STRING'}
 
----OVERVIEW OF DEPENDENCIES---
+|OVERVIEW OF DEPENDENCIES| --
 
 **Client**
+
 REACT - Javasript framework used for front-end display and creating single page application
+
 AXIOS - Used for making HTTP requests
+
 REACT-TOASTIFY - Used to create "toast" notifications on the login page.
+
 REACT-CALENDAR - Used to create calendar for reservation scheduling.
 
 **Server**
+
 EXPRESS - Middleware used with Node.JS to make simplifed and streamlined RESTful API
+
 BCRYPT - Used to securely encrypt user passwords
+
 MONGOOSE - Used to make schemas in MongoDB
+
 NODEMON - Used to start server and automatically apply updates
 
----APP FUNCTIONALITY EXPLAINED---
+|APP FUNCTIONALITY EXPLAINED| --
 1. Make sure that all prerequiste technologies are installed for both the server and the technology (client and server have unique node modules). Type "npm install" in the root directory for the client and "npm install" in the server directory for the server.
 
 2. To use app, the server and client must both be running. To start the server, navigate to the server folder and type "nodemon server.js" (nodemon must be installed with npm). Then, start the client from the root directory type "npm run start."
--- src/views/Login.js
+++ src/views/Login.js
@@ -23,9 +23,11 @@ function Login() {
         const [password, setPassword] = useState("");
         const [confirmPassword, setConfirmPassword] = useState("")
 
-    //Function for when a user clicks register button
+    //Upon clicking register button:
         const registerUser = (e) => {
             e.preventDefault();
+            
+            //Update database using axios
             axios.post('http://localhost:5000/register', {
                 first_name,
                 last_name,
@@ -34,47 +36,112 @@ function Login() {
                 password,
                 confirmPassword
             })
-                .then(res => navigate('/home'))
+
+            //If successful, display toast message and redirect home
+                .then(res => {
+                    toast.success("Success! Logging in.");
+                    setTimeout(() => {navigate("/home")}, 2000)
+                    }
+                )
+            //If errors are found, update error state and display error toast
                 .catch(err =>{
                     console.log("****error*** ",err.response.data.errors);
                     setErrors(err.response.data.errors)
-                })
+                    toast.error('Error: Please correct fields');
+                    }
+                )
         }
 
     //Page Render Using JSX
         return (
-            <div>
-                <NavBar />
-                <Hero back={backGround} title="Welcome" desc="" btnText="RETURN HOME" btnTo="/home"/>
-                <div className={styles.flex}>
-                    <div className={styles.registerdiv}>
-                        <h3>Register</h3>
-                        <form onSubmit={registerUser} className={styles.register}>
-                            <input type="text" placeholder="First Name" onChange={(e) =>setFirstName(e.target.value)} />
-                            {
-                                errors.hasOwnProperty("first_name") ? <p style={{ color: 'red' }}> * {errors["first_name"].message} </p> : ""
-                            }
-                            <input type="text" placeholder="Last Name" onChange={(e) =>setLastName(e.target.value)}/>
-                            {
-                                errors.hasOwnProperty("last_name") ? <p style={{ color: 'red' }}> * {errors["last_name"].message} </p> : ""
-                            }
-                            <input type="text" placeholder="Cell Number/ optional" onChange={(e) =>setNumber(e.target.value)}/>
-                            <input type="text" placeholder="Email" onChange={(e) =>setEmail(e.target.value)}/>
-                            {
-                                errors.hasOwnProperty("email") ? <p style={{ color: 'red' }}> * {errors["email"].message} </p> : ""
-                            }
-                            <input type="password" placeholder="Password" onChange={(e) =>setPassword(e.target.value)} />
-                            {
-                                errors.hasOwnProperty("password") ? <p style={{ color: 'red' }}> * {errors["password"].message} </p> : ""
-                            }
-                            <input type="password" placeholder="Confirm Password" onChange={(e) =>setConfirmPassword(e.target.value)}/>
-                            <input className={styles.logButton} type="submit" value="Create Account" />
-                        </form>
-                    </div>
-                    <LoginComponent/>
+        <div>
+            <ToastContainer /> {/* Enables usage of "toast", which is used in "registerUser function*/}
+            <NavBar />
+            <Hero
+                back={backGround}
+                title="Welcome"
+                desc=""
+                btnText="RETURN HOME"
+                btnTo="/home"
+            />  {/* Background image*/}
+
+            <div className={styles.flex}>
+                <div className={styles.registerdiv}>
+                    <h3>Register</h3>
+                    <form onSubmit={registerUser} className={styles.register}>
+                    <input
+                        type="text"
+                        placeholder="First Name"
+                        onChange={(e) => setFirstName(e.target.value)}
+                    />
+                    {errors.hasOwnProperty("first_name") ? (
+                        <p style={{ color: "red" }}>
+                        {" "}
+                        * {errors["first_name"].message}{" "}
+                        </p>
+                    ) : (
+                        ""
+                    )}
+                    <input
+                        type="text"
+                        placeholder="Last Name"
+                        onChange={(e) => setLastName(e.target.value)}
+                    />
+                    {errors.hasOwnProperty("last_name") ? (
+                        <p style={{ color: "red" }}>
+                        {" "}
+                        * {errors["last_name"].message}{" "}
+                        </p>
+                    ) : (
+                        ""
+                    )}
+                    <input
+                        type="text"
+                        placeholder="Cell Number/ optional"
+                        onChange={(e) => setNumber(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Email"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    {errors.hasOwnProperty("email") ? (
+                        <p style={{ color: "red" }}>
+                        {" "}
+                        * {errors["email"].message}{" "}
+                        </p>
+                    ) : (
+                        ""
+                    )}
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    {errors.hasOwnProperty("password") ? (
+                        <p style={{ color: "red" }}>
+                        {" "}
+                        * {errors["password"].message}{" "}
+                        </p>
+                    ) : (
+                        ""
+                    )}
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                    <input
+                        className={styles.logButton}
+                        type="submit"
+                        value="Create Account"
+                    />
+                    </form>
                 </div>
+                <LoginComponent />
             </div>
-        )
+        </div>
+        );
     }
 
 export default Login
